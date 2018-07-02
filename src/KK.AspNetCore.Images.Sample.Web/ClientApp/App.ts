845e61dbf98f6377ea3d34a7c003f0b31541ab60
kirkone <live@kirk.one>
2018-07-02 20:54:52 +0200

    Update parcel prototype

--- src/KK.AspNetCore.Images.Sample.Web/ClientApp/App.ts
+++
@@ -1,4 +0,0 @@
-import { HelloWorld } from './HelloWorld';
-import './Styles/main.less';
-
-new HelloWorld().doIt();
---
+++ src/KK.AspNetCore.Images.Sample.Web/ClientApp/src/site.ts
@@ -0,0 +1,3 @@
+import { HelloWorld } from './scripts/helloworld';
+
+new HelloWorld().doIt();
